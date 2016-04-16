e295bc0702ecb646567f54f4fcb0695007d2a75e
Dustin <dustinwaggoner@comcast.net>
2016-04-15 19:58:42 -0700

    no real changes yet

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,21 @@
 class ItemsController < ApplicationController
-  def create
+    def create
 
-    # @user = current_user
+        @item = current_user.items.new(item_params)
 
-    @item = current_user.items.new(item_params)
+        if @item.save!
+            flash[:notice] = 'Item saved successfully'
+        else
+            flash[:alert] = 'Item failed to save'
+        end
 
-    # @item.user = current_user
+        redirect_to root_path
+      end
 
-    # @new_item = Item.new
+    private
 
-    if @item.save!
-      flash[:notice] = 'Item saved successfully'
-    else
-      flash[:alert] = 'Item failed to save'
+    def item_params
+        params.require(:item).permit(:name)
     end
 
-    redirect_to root_path
-
-  end
-
-  private
-
-  def item_params
-
-    params.require(:item).permit(:name)
-
-  end
 end
